15f2a3a8a003b95e00d3f05e7f507be6699a885e
Stefan Malinović <malina.stefan39@gmail.com>
2024-05-30 19:39:32 +0200

    new web role

---
+++ backend/RedditCloudService/RSMVC/DTOs/LoginResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSMVC.DTOs
+{
+    public class LoginResponseDTO
+    {
+        //public string Token { get; set; } later
+
+        public string Email { get; set; }
+
+        //public string Image { get; set; } comment
+
+    }
+}
