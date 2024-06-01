153d8abf7053856413cc1a6f53534f5bb032fa13
Stefan Malinović <malina.stefan39@gmail.com>
2024-06-01 15:09:57 +0200

    design navbar

--- backend/RedditCloudService/RSMVC/Controllers/HomeController.cs
+++ backend/RedditCloudService/RSMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RSMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,19 @@ namespace RSMVC.Controllers
 {
     public class HomeController : Controller
     {
+        // Sample data for themes
+        private static List<Theme> themes = new List<Theme>
+        {
+            new Theme { Id = 1, Title = "Dark Mode", Description = "A dark theme for night time browsing.", CreatedDate = DateTime.Now.AddDays(-10) },
+            new Theme { Id = 2, Title = "Light Mode", Description = "A light theme for day time browsing.", CreatedDate = DateTime.Now.AddDays(-5) }
+        };
+
         public ActionResult Index()
         {
+            ViewBag.Themes = themes;
+            //return RedirectToAction("Index", "Home");
             return View();
-        }
+         }
 
         public ActionResult About()
         {
@@ -25,6 +35,18 @@ namespace RSMVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult CreateTheme()
+        {
+            // Logic to create a new theme
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var theme = themes.Find(t => t.Id == id);
+            return View(theme);
         }
     }
 }
